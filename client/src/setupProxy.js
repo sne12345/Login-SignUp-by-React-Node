4672b8ed23c27bef70b7d90c8ffa42af1036f536
naeun <sne12345@naver.com>
2021-02-16 18:36:34 +0900

    daf

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const port = process.env.PORT || 5000
+
 module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: `http://localhost:${port}`,
       changeOrigin: true,
     })
   );
